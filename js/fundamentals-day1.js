a0248e8fb7534a8d7fb362fc31a10da51b5f20ae
Luis Lanza <lanza.luis.lall@gmail.com>
2020-09-09 10:37:26 -0300

    solutions 1st day, a la italian

--- js/fundamentals-day1.js
+++ js/fundamentals-day1.js
@@ -1,1 +1,33 @@
-console.log('up and running');
+function getCount(str) {
+    // let vowelsCount = 0;
+    let arr = str.split('');
+    // console.log(arr);
+    const vowelsCount = (acc, currentValue) => {
+        if (currentValue === 'a') {
+            return acc += 1
+        } else if (currentValue === 'e') { 
+            return acc += 1
+        } else if (currentValue === 'i') {
+            return acc += 1
+        } else if (currentValue === 'o') {
+            return acc += 1
+        } else if (currentValue === 'u') {
+            return acc += 1
+        } else {
+            return acc += 0
+        }
+    };
+    let solution = arr.reduce(vowelsCount, 0)
+    return solution;
+}
+
+console.log(getCount('saab'));
+console.log(getCount('street'));
+console.log(getCount('isi'));
+console.log(getCount('poop'));
+console.log(getCount('uwu'));
+
+// split String /done
+// compare values with desired values (vowels)
+//     when values match ++vowelsCount
+// return vowelsCount
