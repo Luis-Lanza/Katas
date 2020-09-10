7ff4a58729f6cf1e92e6998a1cadb9a6b80c1264
Luis Lanza <lanza.luis.lall@gmail.com>
2020-09-10 13:33:33 -0300

    value calculator kata, unfinished

---
+++ js/fundamentals-day2.js
@@ -0,0 +1,68 @@
+// strings in arrays
+// every character in a string has a value
+// add each value of each string = totalStringValue
+// every position in the array has a value = positionValue
+// multiply that by the position in the array
+
+// assign alphabetical values
+// receive array of strings as param
+// forEach member (for loop)
+//     split string into an array of values
+//     match each character with a value
+//     add values (reduce)
+//     multiply each array member by its index+1
+// return array
+
+
+let arr = arr;
+for (arr.[i]; i < arr.lenght; ++i) {
+    let stringCharacters = str.split('');
+    const foundCharVals = stringCharacters.map(
+        find(
+            element => 
+        )).
+
+    return
+}
+
+str.
+        split('').
+        *map()*.
+        reduce() * arr.[i]
+array of split String (array of characters)
+
+
+
+    
+
+
+
+
+const alphabVal = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+    g: 7,
+    h: 8,
+    i: 9,
+    j: 10,
+    k: 11,
+    l: 12,
+    m: 13,
+    n: 14,
+    o: 15,
+    p: 16,
+    q: 17,
+    r: 18,
+    s: 19,
+    t: 20,
+    u: 21,
+    v: 22,
+    w: 23,
+    x: 24,
+    y: 25,
+    z: 26
+};
